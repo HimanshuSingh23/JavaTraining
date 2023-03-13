e086d8051da8a73242e9b02210ccda16622f50e6
Himanshu Singh <himanshusingh792502gmail.com>
2023-03-13 18:22:49 +0530

    TypeCasting

---
+++ TypeCastingWithExamples.java
@@ -0,0 +1,18 @@
+public class testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=128;
+		double d=i;
+		System.out.println(i);
+		System.out.println(d);
+		System.out.println("The above were done automatically");
+		double e=129.0;
+		int f= (int)e;
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println("The above were done manually");
+
+	}
+
+}
